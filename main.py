1264d273da2e257387576708a950c776d349bc81
MavlonovAbduraxmon <mavlonovabduraxmon544@gmail.com>
2025-07-22 10:40:45 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+
+
+"""
+git config --global user.name "MavlonovAbduraxmon"
+git config --global user.email "mavlonovabduraxmon544@gmail.com"
+"""
+
+"""
+git add .
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/MavlonovAbduraxmon/Githubproject1.git
+git push -u origin master
+"""
